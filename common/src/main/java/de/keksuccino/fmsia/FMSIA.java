d9a08c24bf815067689fc9bd785db094aa025c59
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-05-07 05:26:14 +0200

    ---

--- common/src/main/java/de/keksuccino/fmsia/FMSIA.java
+++ common/src/main/java/de/keksuccino/fmsia/FMSIA.java
@@ -14,7 +14,7 @@ public class FMSIA {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "fmsia";
 	public static final String MSG_PREFIX = "[FANCYMENU SYSTEM INTERACTIONS ADDON]";
